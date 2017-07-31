9ac6a81daa980803d157d9878e41f3a5e400cc0f
Vitaly Lipatov <lav@etersoft.ru>
2017-07-31 12:32:47 +0300

    fix convert

--- convert.sh
+++ convert.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 rm -rf auc/
 mkdir -p auc/
+
+print_cert()
+{
+    local name="$1"
+    local file="$2"
+    printf '#\n# %s\n#\n\n' "$name"
+    openssl x509 -inform der -sha256 -nameopt utf8 -in $file -text -fingerprint
+    printf '\n\n'
+}
+
+rm -f ca-gost-bundle.crt
 xsltproc tslext.xsl TSLExt.1.0.xml | while read name data ; do
     echo "$data" | base64 -d > auc/$name.crt
+    print_cert $name auc/$name.crt >>ca-gost-bundle.crt
 done
